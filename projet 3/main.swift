79053df8999a06c6c9236da043f55ae5d89af03b
soso <succo13@yahoo.fr>
2017-09-13 19:13:45 +0200

    Initial Commit

---
+++ projet 3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  projet 3
+//
+//  Created by vador on 13/09/2017.
+//  Copyright © 2017 sofiane bahri. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
